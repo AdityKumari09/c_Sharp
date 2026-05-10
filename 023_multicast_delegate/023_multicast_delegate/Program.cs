3bc6a82218516e2072001ccca92b3811002001a2
AdityKumari09 <kumariadity09@gmail.com>
2026-05-10 08:16:25 +0530

    multicast_delegate

---
+++ 023_multicast_delegate/023_multicast_delegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _023_multicast_delegate
+{
+    internal class Program
+    {
+        public delegate int SampleDelegate();
+
+        public static int SampleMethodOne()
+        {
+            return 1;
+        }
+
+        public static int SampleMethodTwo()
+        {
+            return 2;
+        }
+        static void Main(string[] args)
+        {
+            SampleDelegate d = new SampleDelegate(SampleMethodOne);
+            d += SampleMethodTwo;
+
+            int DelegateReturnValue = d();
+
+            Console.WriteLine("Delegate returned values is {0}", DelegateReturnValue);
+        }
+    }
+}
