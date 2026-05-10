bd4867f0e0151f14b4a48ca61931e9db745ea179
AdityKumari09 <kumariadity09@gmail.com>
2026-05-10 09:10:28 +0530

    committed

---
+++ 024_exception_handling/024_exception_handling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024_exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader str = null;
+
+            try
+            {
+                str = new StreamReader(@"C:\Sample Files\Data.txt");
+                Console.WriteLine(str.ReadToEnd());
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Please check if the file {0} exists", ex.FileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (str != null)
+                {
+                    str.Close();
+                }
+                Console.WriteLine("Finally block executed");
+            }
+        }
+    }
+}
