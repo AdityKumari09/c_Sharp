0922e5ce3fffe7ee52488ab9d321c47949729ecb
AdityKumari09 <kumariadity09@gmail.com>
2026-05-10 08:02:22 +0530

    Delegates

---
+++ 022_Delegates/022_Delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _022_Delegates
+{
+    internal class Program
+    {
+        delegate void demoDelegateFun(string x, int y);
+
+        public static void printDetails(string name, int age)
+        {
+            Console.WriteLine($"Name = {name}");
+            Console.WriteLine($"Age = {age}");
+        }
+
+        public static void PrintStringNTimes(string x, int y)
+        {
+            for (int i = 0; i < y; i++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+        static void Main(string[] args)
+        {
+            demoDelegateFun demo;
+
+            demo = new demoDelegateFun(printDetails);
+            demo("Adity", 21);
+
+            demo = new demoDelegateFun(PrintStringNTimes);
+            demo("CTS", 5);
+
+            Console.ReadKey();
+        }
+    }
+}
