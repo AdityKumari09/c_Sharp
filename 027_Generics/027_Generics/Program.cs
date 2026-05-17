fb26278b21562d4b7a63308ae8ac0cb61a9afab9
AdityKumari09 <kumariadity09@gmail.com>
2026-05-18 00:04:30 +0530

    generics

---
+++ 027_Generics/027_Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _027_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Equal = Calculator<int>.AreEqual(10, 20);
+            if (Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+                Console.WriteLine("Not equal");
+        }
+    }
+
+    public class Calculator<T>
+    {
+        public static bool AreEqual(T value1, T value2)
+        {
+            return value1.Equals(value2);
+        }
+    }
+}
---
+++ 027_Generics/027_Generics/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("027_Generics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("027_Generics")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7056ab3-cbec-4fd6-872f-100b3b9c070e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
